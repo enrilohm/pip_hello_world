0b2c02f0a782546b01e8926ef546351b33c2a883
Enrico Lohmann <enrico.lohmann@mercateo.com>
2020-04-30 18:14:38 +0200

    update data_path logic

--- pip_hello_world/__init__.py
+++ pip_hello_world/__init__.py
@@ -1,6 +1,13 @@
 from pip_hello_world.pip_hello_world import *
+
 import os
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-def get_path(path):
-    return os.path.join(_ROOT, 'data', path)
+
+
+def get_data_path(path):
+    return os.path.join(_ROOT, "data", path)
+
+
+def get_source_path(path):
+    return os.path.join(_ROOT, path)
